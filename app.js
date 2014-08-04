cae859abcdf210ccd731c244f16ea6f6aa01e4c5
Kevin Bielawski <kbielawsk7@gmail.com>
2014-08-04 09:52:50 -0400

    logfmt

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    logfmt = require("logfmt"),
+    app = express();
+
+app.use(logfmt.requestLogger());
 
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
@@ -8,7 +11,7 @@ app.get('*', function(req, res){
   res.sendfile('./index.html');
 });
 
-var port = 5000;
+var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log('Listening on ' + port);
+  console.log("Listening on " + port);
 });
